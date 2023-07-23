52873241ec8a0ff12e75a6bd8546854db9a74fc1
vaneIv <appvane@gmail.com>
2023-07-23 22:52:01 +0200

    Created ArticleDetailsFragment, SearchNewsFragment classes and layouts.

---
+++ app/src/main/java/com/example/thefortnightly/ui/ArticleDetailsFragment.kt
@@ -0,0 +1,7 @@
+package com.example.thefortnightly.ui
+
+import androidx.fragment.app.Fragment
+import com.example.thefortnightly.R
+
+class ArticleDetailsFragment : Fragment(R.layout.fragment_article_details) {
+}
---
+++ app/src/main/java/com/example/thefortnightly/ui/search/SearchNewsFragment.kt
@@ -0,0 +1,7 @@
+package com.example.thefortnightly.ui.search
+
+import androidx.fragment.app.Fragment
+import com.example.thefortnightly.R
+
+class SearchNewsFragment : Fragment(R.layout.fragment_search_news) {
+}
